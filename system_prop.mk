93e4785f6d7501dc954684ad14eeb79e2e56dee3
Ethan Chen <intervigil@gmail.com>
2014-09-22 16:27:28 -0700

    g3-common: Add system properties

--- system_prop.mk
+++ system_prop.mk
@@ -1,14 +1,35 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=fluence \
-    persist.audio.fluence.mode=endfire \
-    persist.audio.headset_fluence=false \
+    audio.offload.24bit.enable=false \
+    audio.offload.buffer.size.kb=32 \
+    audio.offload.gapless.enabled=false \
+    audio.offload.multiple.enabled=false \
+    audio.offload.pcm.enable=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    av.offload.enable=false \
+    av.streaming.offload.enable=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    mm.enable.smoothstreaming=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.audio.fluencetype=fluence
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    use.dedicated.device.for.voip=true \
     use.voice.path.for.pcm.voip=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=dyn \
     persist.hwc.mdpcomp.enable=true \
-    ro.qualcomm.cabl=0 \
+    ro.opengles.version=196608 \
     ro.sf.lcd_density=640
 
 # GPS
@@ -19,13 +40,40 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.izat.premium_enabled=0 \
     ro.qc.sdk.izat.service_mask=0x0
 
+# NITZ
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
+
+# Qualcomm
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true \
+    qcom.hw.aac.encoder=true \
+    ro.qualcomm.cabl=0 \
+    ro.vendor.extension_library=/vendor/lib/libqc-opt.so
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.use_data_netmgrd=true \
-    persist.data.netmgrd.qos.enable=false \
     persist.radio.add_power_save=1 \
-    ro.telephony.ril_class=LgeLteRIL
+    ro.telephony.ril_class=LgeLteRIL \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.netmgrd.qos.enable=false \
+    ro.use_data_netmgrd=true
+
+# Recovery
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cwm.forbid_format=/boot,/firmware,/mpt,/persist,/persist-lg,/sns
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.qualcomm.sns.daemon=e \
+    debug.qualcomm.sns.hal=e \
     debug.qualcomm.sns.libsensor1=e
