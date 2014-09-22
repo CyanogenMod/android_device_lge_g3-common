61f6b37d7a939ff0986012f2547aca35d7321af1
Ethan Chen <intervigil@gmail.com>
2014-09-22 16:16:00 -0700

    g3-common: Set GPS properties

--- system_prop.mk
+++ system_prop.mk
@@ -12,6 +12,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=640
 
 # GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qc_nlp_in_use=1 \
+    persist.loc.nlp_name=com.qualcomm.services.location \
+    ro.gps.agps_provider=1 \
+    ro.qc.sdk.izat.premium_enabled=0 \
+    ro.qc.sdk.izat.service_mask=0x0
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
