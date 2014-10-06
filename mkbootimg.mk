24a81422fa93732ee13311a8d186e49193ff72de
Ethan Chen <intervigil@gmail.com>
2014-10-06 15:22:20 -0700

    g3-common: Remove more duplicate kernel scripting

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,40 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
-## THIS IS A DEFAULT: YOU SHOULD OVERRIDE IT FROM THE DEVICE-SPECIFIC
-## BoardConfig. Check the kernel's arch/arm/boot/dts/ path for possible
-## values.
-CM_DTS_TARGET ?= msm8974pro-g3-
-
-
-## Don't change anything under here. The variables are named CM_whatever
-## on purpose, to avoid conflicts with similarly named variables at other
-## parts of the build environment
-
-## Imported from the original makefile...
-KERNEL_CONFIG := $(KERNEL_OUT)/.config
-CM_DTS_NAMES := msm8974
-
-CM_DTS_FILES = $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/$(CM_DTS_TARGET)*/*.dts)
-CM_DTS_FILE = $(lastword $(subst /, ,$(1)))
-DTB_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%.dtb,$(call CM_DTS_FILE,$(1))))
-ZIMG_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%-zImage,$(call CM_DTS_FILE,$(1))))
-KERNEL_ZIMG = $(KERNEL_OUT)/arch/arm/boot/zImage
-
-define append-cm-dtb
-mkdir -p $(KERNEL_OUT)/arch/arm/boot;\
-$(foreach CM_DTS_NAME, $(CM_DTS_NAMES), \
-   $(foreach d, $(CM_DTS_FILES), \
-      cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));))
-endef
-
-
 ## Build and run dtbtool
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
-	$(call append-cm-dtb)
 	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
 	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
