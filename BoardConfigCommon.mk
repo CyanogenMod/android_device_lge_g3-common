99351dbf678c6bafcf2aff008742e5191acd017d
Dan Pasanen <dan.pasanen@gmail.com>
2014-10-24 14:26:50 -0500

    camera: add prop permissions
    Change-Id: Icf42bd37c713a80c2b0b317515d3402e8617b449

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,6 +55,12 @@ BOARD_HAVE_BLUETOOTH := true
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 COMMON_GLOBAL_CFLAGS += -DLG_CAMERA_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND=' \
+    { "persist.data.sensor_name", AID_CAMERA, 0 }, \
+    { "camera.4k2k.enable", AID_MEDIA, 0 }, \
+    { "persist.data.rear.minfps", AID_MEDIA, 0 }, \
+    { "persist.data.front.minfps", AID_MEDIA, 0 }, \
+    '
 
 # Classpath
 PRODUCT_BOOT_JARS := $(subst $(space),:,$(PRODUCT_BOOT_JARS))
