d6cd07f59f7c77735e7ae1be53c6c9edd33b92b2
Dan Pasanen <dan.pasanen@gmail.com>
2014-10-30 22:41:07 -0700

    g3-common: Add LS990 to build barrier
    Change-Id: Id3d70e22e4796f0bd266a1dff83004d2b4890e83

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter g3 d851 vs985, $(TARGET_DEVICE)),)
+ifneq ($(filter g3 d851 ls990 vs985, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
