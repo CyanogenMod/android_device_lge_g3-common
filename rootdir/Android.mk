ab25d17c70edf823c02a494cc079fee0ff07847d
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-02 20:47:09 -0600

    g3: de-commonize fstab
    * d851 uses msm_sdcc.3 while vs985 uses msm_sdcc.2...*sigh*
    
    Change-Id: I5143932faa16f32e10e3a1627176bfbba6e26ef3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,14 +19,6 @@ include $(BUILD_PREBUILT)
 # Init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := fstab.g3
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/fstab.g3
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE            := init.g3.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
