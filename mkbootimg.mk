604ed9002e65e2b12ccc8481d10f61b53c1f0cb1
Anthony King <anthonydking@gmail.com>
2014-11-09 23:46:02 -0600

    g3: add bump script
    * auto bumps boot and recovery images
    * to enable for your variant, set TARGET_REQUIRES_BUMP in BoardConfig
    
    Change-Id: Ib04dda56564c8b89f9c24a75ef5dffc43073b063

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ## Build and run dtbtool
+BUMP := $(LOCAL_PATH)/bump/bump.py
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
@@ -16,6 +17,9 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTAL
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+ifeq ($(TARGET_REQUIRES_BUMP),true)
+	$(hide) $(BUMP) $@ $@
+endif
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
 ## Overload recoveryimg generation: Same as the original, + --dt arg
@@ -25,4 +29,7 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+ifeq ($(TARGET_REQUIRES_BUMP),true)
+	$(hide) $(BUMP) $@ $@
+endif
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
