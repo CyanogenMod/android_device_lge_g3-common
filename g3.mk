205b4c4078bce0402945fb6a81ef58d2892245df
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-26 20:19:16 -0600

    g3: split mixer_paths for bcm/wcnss
    Change-Id: Ic29f8420297f55091736d04cb4dece30ac6cbe4c

--- g3.mk
+++ g3.mk
@@ -82,8 +82,7 @@ PRODUCT_PACKAGES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Bluetooth
 PRODUCT_PACKAGES += \
