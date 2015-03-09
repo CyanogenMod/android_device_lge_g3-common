fe1a38fead6b5f9cf058af91b41635a7ed7a4395
Dan Pasanen <dan.pasanen@gmail.com>
2015-03-08 21:25:48 -0500

    g3: enable otg storage settings
    Change-Id: Ida7a453dcdc4f3977f23899e32f57da4887926fb

--- system_prop.mk
+++ system_prop.mk
@@ -76,3 +76,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.qualcomm.sns.daemon=e \
     debug.qualcomm.sns.hal=e \
     debug.qualcomm.sns.libsensor1=e
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.isUsbOtgEnabled=true
