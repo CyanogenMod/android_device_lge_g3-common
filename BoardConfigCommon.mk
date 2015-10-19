2648081330d782eb08770eb7c46bfc18adda127b
Steve Kondik <shade@chemlab.org>
2015-10-19 17:46:46 -0500

    g3: Support for native tap-to-wake
    Change-Id: I80998dbe631468be269c6f7d8e2284111b7d245d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,6 +66,7 @@ COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND=' \
 
 # CMHW
 BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/lpwg_notify"
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
