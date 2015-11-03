c9caaa4f29300e57c53dfd2cebd57af6ab84df75
Hendrik H <git@finnq.de>
2015-11-03 18:01:29 +0100

    g3: Add boringssl-compat wrapper
    Change-Id: I1650b3e9e7c1de23211d6fb9c597f52b5d1867c4

--- g3.mk
+++ g3.mk
@@ -89,6 +89,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     bdAddrLoader
 
+# BoringSSL compatability wrapper
+PRODUCT_PACKAGES += \
+    libboringssl-compat
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8974
