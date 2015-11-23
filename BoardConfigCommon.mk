2a8446ed69ae9728d90f7534b39e7b49a5e9e256
Ethan Chen <intervigil@gmail.com>
2015-11-22 20:39:55 -0600

    g3: remove custom ril class
    * GSM variants don't need this, move variant-specific hacks down
      to variant trees.
    
    Change-Id: Id97cd8d03a02fa77191cfaa1c711af10d376b785

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,9 +114,6 @@ COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 
-# Radio
-BOARD_RIL_CLASS := ../../../device/lge/g3-common/ril/
-
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
--- system_prop.mk
+++ system_prop.mk
@@ -57,8 +57,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.add_power_save=1 \
     persist.radio.apm_sim_not_pwdn=1 \
-    ro.ril.telephony.mqanelements=5 \
-    ro.telephony.ril_class=LgeLteRIL
+    ro.ril.telephony.mqanelements=5
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=false \
