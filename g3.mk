da8583436dbf1ab986cfb3736f813e74d3ea05cf
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-02-02 14:14:15 -0600

    g3: add audio effects config and qc libs
    Change-Id: I775cda1d8a0f2dff04ef939d3876f6632eae8267

--- g3.mk
+++ g3.mk
@@ -82,6 +82,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Bluetooth
